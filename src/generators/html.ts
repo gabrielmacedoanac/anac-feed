134237763f33196a1fdebdabe7e5d0703410438a
gabrielmacedoanac <83769557+gabrielmacedoanac@users.noreply.github.com>
2025-04-30 18:00:46 -0300

    color "notícia"

--- src/generators/html.ts
+++ src/generators/html.ts
@@ -125,7 +125,7 @@ export async function generateSemanticHtml(conteudos: ContentItem[], outputPath:
     }
     
     .type-notícia {
-      background-color:rgb(163, 223, 255);
+      background-color:rgb(206, 238, 255);
       color: var(--primary);
     }
     
